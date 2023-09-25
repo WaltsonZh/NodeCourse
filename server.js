e3eedf841b9c70aa11e7bad1e63030cf83c66746
adamaconguli <waltson2003@gmail.com>
2023-09-25 21:41:38 +0800

    lesson 7: middleware

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 
 const PORT = process.env.PORT || 6942
+
+// custom middleware logger
+app.use(logger)
+
+// Cross Origin Resource Sharing
+const whitelist = ['http://localhost:6942']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+// built-in middleware to handle urlencoded data
+app.use(express.urlencoded({ extended: false }))
+
+// built-in middleware for json
+app.use(express.json())
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('^/$|/index(.html)?', (req, res) => {
   // res.sendFile('./views/index.html', { root: __dirname })
@@ -18,12 +47,16 @@ app.get('/old-page.html', (req, res) => {
 })
 
 // route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-  console.log('attempted to load hello.html')
-  next()
-}, (req, res) => {
-  res.send('Hello World!')
-})
+app.get(
+  '/hello(.html)?',
+  (req, res, next) => {
+    console.log('attempted to load hello.html')
+    next()
+  },
+  (req, res) => {
+    res.send('Hello World!')
+  }
+)
 
 // chaining route handlers
 const one = (req, res, next) => {
@@ -43,8 +76,18 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three])
 
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+// app.use('/) <- does not accept regex
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' })
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
