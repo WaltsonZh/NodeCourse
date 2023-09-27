6b136981cfc02d5c71be0495d82dda73a97aae17
adamaconguli <waltson2003@gmail.com>
2023-09-27 21:52:58 +0800

    chapter 13: intro to mongodb & mongoose

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const app = express()
@@ -8,8 +9,13 @@ const errorHandler = require('./middleware/errorHandler')
 const verifyJWT = require('./middleware/verifyJWT')
 const cookieParser = require('cookie-parser')
 const credentials = require('./middleware/credentials')
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConn')
 
 const PORT = process.env.PORT || 6942
+
+// connect to MongoDB
+connectDB()
 
 // custom middleware logger
 app.use(logger)
@@ -53,4 +59,7 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
