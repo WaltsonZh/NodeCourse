51510508b05304f183b78702c4741834da7b14b0
adamaconguli <waltson2003@gmail.com>
2023-09-28 20:31:48 +0800

    course completed

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const usersController = require('../../controllers/usersController')
+const router = express.Router()
+const ROLES_LIST = require('../../config/rolesList')
+const verifyRoles = require('../../middleware/verifyRoles')
+
+router.route('/')
+  .get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers)
+  .delete(verifyRoles(ROLES_LIST.Admin), usersController.deleteUser)
+
+router.route('/:id').get(verifyRoles(ROLES_LIST.Admin), usersController.getUser)
+
+module.exports = router
